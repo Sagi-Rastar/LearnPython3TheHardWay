8c2949f438f5264f31c5ab59c6a9197372573d41
sagi-rastar <sagirastar@gmail.com>
2022-08-13 18:51:46 +0800

    提交ex2，ex3 优化文件结构 zh你下一章文件记得放文件夹

---
+++ dcy/dcy_ex3.py
@@ -0,0 +1,28 @@
+#Why?
+#Why not?
+print("I will now count my chickens:")
+
+#计算母鸡只数
+print ("Hens", 25 + 30 / 6)
+#计算公鸡只数
+print ("Roosters", 100 - 25 * 3 % 4)
+#他要数鸡蛋了
+print ("Now I will count the eggs:")
+#这么多鸡蛋
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5- 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+#date：2022年8月13日18:49:04
