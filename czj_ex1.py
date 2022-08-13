96e1ef910f2d3b6383023a38bcd4b7643a738bcc
FlowsMple <88260862+FlowsMple@users.noreply.github.com>
2022-08-13 16:41:36 +0800

    Delete czj_ex1.py

--- czj_ex1.py
+++
@@ -1,8 +0,0 @@
-print("hello world",end = ' ')
-print("Hello again",end = ' ')
-print("I like typing this",end = ' ')
-print("This is fun",end = ' ')
-print('Yay!Printing',end = ' ')
-print("I'd much rather you 'not'.",end = ' ')
-print('I"said" do not touch this .',end = ' ')
-#aQUA!
