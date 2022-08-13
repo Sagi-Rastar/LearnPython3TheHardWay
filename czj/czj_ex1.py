69394157942697549b4cb4123b0bd40a0688dea9
FlowsMple <88260862+FlowsMple@users.noreply.github.com>
2022-08-13 16:42:00 +0800

    Add files via upload

---
+++ czj/czj_ex1.py
@@ -0,0 +1,8 @@
+print("hello world",end = ' ')
+print("Hello again",end = ' ')
+print("I like typing this",end = ' ')
+print("This is fun",end = ' ')
+print('Yay!Printing',end = ' ')
+print("I'd much rather you 'not'.",end = ' ')
+print('I"said" do not touch this .',end = ' ')
+#aQUA!
---
+++ czj/czj_ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+#计算母鸡数
+print("Hens", 25 + 30 / 6)
+#计算公鸡数
+print("Roosters", 100 - 25 * 3 % 4)
+print("Now I will count the eggs:")
+#计算蛋数
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+print("How about some more.")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+#python用来计算组合数真是挺快的^ ^
+import math
+print(math.comb(128, 12))
