0dbc8f3a81adb38af011c39faca72df0c77fe2b9
sagi-rastar <sagirastar@gmail.com>
2022-08-15 00:33:17 +0800

    修改ex5

--- dcy/dcy_ex5.py
+++ dcy/dcy_ex5.py
@@ -1,6 +1,3 @@
-from re import T
-
-
 my_name = 'dcy'
 my_age = 19 # 20 this year
 my_height = 175 # meter
@@ -17,6 +14,8 @@ print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.") # yeeeee
 
 #this line is tricky, try to get it sxactly right
-total = my_age + my_height + my_weight
+total = round(my_age + my_height + my_weight,2)
+#嘻嘻摸鱼就不写转换了！
+#round(x,y) | x = 被舍入数 ， y = 保留位数
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
 #捏妈我还以为等号前后有空格是python严格缩进限制的
