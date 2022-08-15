5b3b11b0ee254a27effa1118e252d5d37144cf23
ZEXMKER <719958601@QQ.COM>
2022-08-15 15:20:19 +0800

    提交ex2~5,添加个人readme(必看）

---
+++ ZH/zh_ex5.py
@@ -0,0 +1,11 @@
+my_name='ZH'#相当于给变量赋了字符，字符串用单引号表示
+my_age=19
+my_height=175 #miles
+my_weight=68 #g
+my_eyes='black'
+my_teeth='white'
+my_hair='brown'
+
+print(f"let's talk about {my_name}.")
+#在同一段引用内容里若想输出变量内容，则用花括号，在此之前要在引用双引号之前加小写f，表示格式化
+#若不加f 则原样输出
