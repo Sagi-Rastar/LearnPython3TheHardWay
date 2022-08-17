129cd5629fe2e54ea2673053d491cbbd8af3bf48
FlowsMple <88260862+FlowsMple@users.noreply.github.com>
2022-08-17 23:42:28 +0800

    昨天cf上分了，好耶！

---
+++ czj/czj_ex11.py
@@ -0,0 +1,17 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print("吻必秒蒜还是蒜必秒吻？",end = '')
+answer = input()
+if(answer == "吻必秒蒜"):
+    print("好耶")
+else:
+    if(answer == "蒜必秒吻"):
+        print("异端！！！")
+    else:
+        print("双选题都不会选，赶紧找个厂吧")
+
---
+++ czj/czj_ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+#用三个及以上双引号引起来就能无视换行随便乱写
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
