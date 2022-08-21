eae5a4857193a4da1f51205cb0641eb51ecf3a42
FlowsMple <88260862+FlowsMple@users.noreply.github.com>
2022-08-22 00:32:38 +0800

    Add files via upload

---
+++ czj/czj_ex18.py
@@ -0,0 +1,33 @@
+#设计函数参数时，如果使用两个**符号后跟字母，表示这个参数是一个列表
+def print_two(**args):
+    tmp = args
+    print(tmp)
+
+#我们在设计函数参数的时候，如果使用一个*符号后跟字母，表示这是一个元组
+def print_w(x,*args):
+    tmp = args
+    print(tmp)
+    print(tmp[0])
+    print(x)
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothin'.")
+
+a = "Zed";b = "Shaw";c = "flow"
+ar = (a,b,c)
+br = (a,c,b)
+
+print_two(a = 1,b = 2,c = 3)
+print_w(a,b,c)
+print_w(ar,br)
+
+print_two_again(a,b)
+print_one(a)
+print_none()
