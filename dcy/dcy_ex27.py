e3a545c6bb026aaeaa9f8eb495a54a69a4a744f5
sagi-rastar <sagirastar@gmail.com>
2022-08-25 14:34:13 +0800

    提交ex26-28，更新个人rm

---
+++ dcy/dcy_ex27.py
@@ -0,0 +1,38 @@
+#Logic
+#filename:dcy_ex27.py
+'''
+• and
+• or
+• not
+• != （不等于）
+• == （等于）
+• >= （大于等于）
+• <= （小于等于）
+• True
+• False
+'''
+'''
+The Truth Tables
+我们现在要用这些字符来帮你记忆 truth tables 的内容。
+
+NOT	True?
+not False	True
+not True	False
+OR	True?
+True or False	True
+True or True	True
+False or True	True
+False or False	False
+AND	True?
+True and False	False
+True and True	True
+False and True	False
+False and False	False
+NOT OR	True?
+not (True or False)	False
+not (True or True)	False
+not (False or True)	False
+not (False or False)	True
+
+'''
+
